0b6e7b9cc255be2e0c28ddef918e1ac23bf2e554
Haider <haiderrehman509@gmail.com>
2026-09-11 16:52:39 +0500

    separating files

---
+++ lib/input_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("BMI calculator"),
+      ),
+      body: Center(child: Text("Body Text")),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,21 @@
+import 'package:bmi_calculator_flutter/input_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const InputPage());
+  runApp(const StartPage());
 }
 
-class InputPage extends StatelessWidget {
-  const InputPage({super.key});
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("BMI calculator"),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        appBarTheme: AppBarTheme(backgroundColor: Color(0xFF0A0E21)),
       ),
-      body: Center(child: Text("Body Text")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            // Increment the counter when the button is pressed
-          });
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: InputPage(),
     );
   }
 }
